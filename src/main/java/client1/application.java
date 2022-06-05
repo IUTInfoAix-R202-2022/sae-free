1062e44e1e532477ff8337e7b965a8487445c1bb
EnzoMEKERKE <enzo.mekerke@etu.univ-amu.fr>
2022-06-05 23:16:16 +0200

    application

--- src/main/java/client1/application.java
+++ src/main/java/client1/application.java
@@ -2,12 +2,16 @@ package client1;
 
 import java.io.IOException;
 import java.lang.*;
+
+import fr.univ_amu.iut.view.map.France;
+import fr.univ_amu.iut.view.map.FranceBuilder;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class application extends Application {
@@ -20,14 +24,25 @@ public class application extends Application {
     public void start(Stage primaryStage) throws Exception{
 
             primaryStage.setTitle("Application DNE");
-            Parent root = FXMLLoader.load(getClass().getResource("application.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("Accueil.fxml"));
             primaryStage.setScene(new Scene(root, 600,400));
-            primaryStage.setResizable(false);
+
+            //primaryStage.setResizable(false);
+        France carte = new France();
+
+        AnchorPane carte1 = new AnchorPane(carte);
+        //.backgroundColor(Color.web("#4aa9d7"))
+        //.fillColor(Color.web("#dcb36c"))
+        //.strokeColor(Color.web("#987028"))
+        //.hoverColor(Color.web("#fec47e"))
+        //.pressedColor(Color.web("#6cee85"))
+        //.selectedColor(Color.MAGENTA)
+
             primaryStage.show();
         }
 
     public void buttonclicked (ActionEvent event) throws IOException {
-        Parent loader = FXMLLoader.load(getClass().getResource("besoinAide.fxml"));
+        Parent loader = FXMLLoader.load(getClass().getResource("BesoinAide.fxml"));
         Scene scene = new Scene(loader);
         Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         app_stage.setScene(scene);
@@ -35,14 +50,25 @@ public class application extends Application {
         app_stage.show();
     }
 
-    public void buttonClickedFormulaire (ActionEvent event) throws IOException {
-        Parent loader = FXMLLoader.load(getClass().getResource("formulaire.fxml"));
+    public void buttonclickedReseaux (ActionEvent event) throws IOException {
+        Parent loader = FXMLLoader.load(getClass().getResource("réseaux.fxml"));
         Scene scene = new Scene(loader);
         Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         app_stage.setScene(scene);
         app_stage.setResizable(false);
         app_stage.show();
     }
+
+
+    public void buttonClickedFormulaire (ActionEvent event) throws IOException {
+        Parent loader = FXMLLoader.load(getClass().getResource("Formulaire.fxml"));
+        Scene scene = new Scene(loader);
+        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.setScene(scene);
+        app_stage.setResizable(false);
+        app_stage.show();
+    }
+
 
     public void buttonClickedReturn (ActionEvent event) throws IOException {
         Parent loader = FXMLLoader.load(getClass().getResource("application.fxml"));
