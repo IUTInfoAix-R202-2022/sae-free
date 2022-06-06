6a49eec6fd3b1e732b2b63ac25f4acad40fec721
EnzoMEKERKE <mekerkenzo@gmail.com>
2022-06-06 18:59:08 +0200

    application

---
+++ src/main/java/client1/Recherches.java
@@ -0,0 +1,35 @@
+package client1;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.sql.*;
+import java.util.ResourceBundle;
+
+import static javafx.application.Application.launch;
+
+public class Recherches implements Initializable {
+
+    public static void main(String[]args){
+        launch(args);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        final String CONNECT_URL = "jdbc:postgresql://castor.db.elephantsql.com/ldtefamu";
+        final String LOGIN = "ldtefamu";
+        final String PASSWORD = "P9-z5ao1AZ5iPG9X3QBkbZNr3-w725g6";
+
+        try {
+            Connection con = DriverManager.getConnection(CONNECT_URL, LOGIN, PASSWORD);
+            Statement statement = con.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select p from Academie p  ");
+
+            System.out.println(resultSet);
+        } catch (SQLException e) {
+
+        }
+    }
+}
---
+++ src/test/java/fr/univ_amu/iut/ApplicationTest.java
@@ -0,0 +1,19 @@
+package fr.univ_amu.iut;
+import client1.application;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.framework.junit5.Start;
+
+import java.awt.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.util.NodeQueryUtils.hasText;
+
+@ExtendWith(ApplicationExtension.class)
+public class ApplicationTest {
+}
